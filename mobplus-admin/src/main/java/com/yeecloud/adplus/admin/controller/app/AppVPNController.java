09d874ce46381073f0a5b3bf831a0e64100f4727
BlackAndW <2586127609@qq.com>
2021-02-25 10:52:13 +0800

    小修改

--- mobplus-admin/src/main/java/com/yeecloud/adplus/admin/controller/app/AppVPNController.java
+++ mobplus-admin/src/main/java/com/yeecloud/adplus/admin/controller/app/AppVPNController.java
@@ -9,11 +9,19 @@ import com.yeecloud.meeto.common.util.HttpUtils;
 import com.yeecloud.meeto.common.util.PageInfo;
 import com.yeecloud.meeto.common.util.Query;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 
 /**
@@ -27,26 +35,72 @@ import java.util.Map;
 @RequestMapping("/api/app/vpn")
 public class AppVPNController {
 
-    private final static String VPN_URL = "http://localhost:9092";
+    private final static String VPN_URL = "https://api.turbovpns.com";
+//    private final static String VPN_URL = "http://localhost:9092";
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null,  new TrustManager[] { new TrustAllCerts() }, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
+
+    private static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {return new X509Certificate[0];}
+    }
+
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
 
     @GetMapping
     @RequiresPermissions("app:config:query")
-    public Result list() throws ServiceException {
-        JSONObject jsonObject = JSONObject.parseObject(HttpUtils.get(VPN_URL + "/app/api/v1/c03/list"));
+    public Result list() throws ServiceException, IOException {
+        final Request request = new Request.Builder().url(VPN_URL + "/app/api/v1/c03/list").get().build();
+        Response response = buildNoVerifyClient().newCall(request).execute();
+        JSONObject jsonObject = JSONObject.parseObject(response.body().string());
         return Result.SUCCESS(getDataResult(jsonObject));
     }
 
     @PostMapping
     @RequiresPermissions("app:config:create")
-    public Result create(@RequestBody Map<String, Object> params) throws ServiceException {
-        HttpUtils.postJson(VPN_URL + "/app/api/v1/c03/create", JSONObject.toJSONString(params));
+    public Result create(@RequestBody Map<String, Object> params) throws ServiceException, IOException {
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        final Request request = new Request.Builder().url(VPN_URL + "/app/api/v1/c03/create")
+                .post(okhttp3.RequestBody.create(mediaType, JSONObject.toJSONString(params)))
+                .build();
+        buildNoVerifyClient().newCall(request).execute();
+//        HttpUtils.postJson(VPN_URL + "/app/api/v1/c03/create", JSONObject.toJSONString(params));
         return Result.SUCCESS();
     }
 
     @PutMapping("/{id}")
     @RequiresPermissions("app:config:edit")
-    public Result update(@PathVariable Integer id, @RequestBody Map<String, Object> params) throws ServiceException {
-        HttpUtils.postJson(VPN_URL + "/app/api/v1/c03/update/" + id, JSONObject.toJSONString(params));
+    public Result update(@PathVariable Integer id, @RequestBody Map<String, Object> params) throws ServiceException, IOException {
+        System.out.println(params);
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        final Request request = new Request.Builder().url(VPN_URL + "/app/api/v1/c03/update/" + id)
+                .post(okhttp3.RequestBody.create(mediaType, JSONObject.toJSONString(params)))
+                .build();
+        buildNoVerifyClient().newCall(request).execute();
+//        HttpUtils.postJson(VPN_URL + "/app/api/v1/c03/update/" + id, JSONObject.toJSONString(params));
         return Result.SUCCESS();
     }
 
@@ -59,5 +113,12 @@ public class AppVPNController {
         data.put("data", object.getJSONArray("result"));
         System.out.println(data);
         return data;
+    }
+
+    private OkHttpClient buildNoVerifyClient () {
+        OkHttpClient.Builder mBuilder = new OkHttpClient.Builder();
+        mBuilder.sslSocketFactory(createSSLSocketFactory());
+        mBuilder.hostnameVerifier(new TrustAllHostnameVerifier());
+        return mBuilder.build();
     }
 }
