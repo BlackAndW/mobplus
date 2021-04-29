bbcdae2233574bde4c1148f64b747ff1e432bd2c
BlackAndW <2586127609@qq.com>
2021-04-29 15:16:40 +0800

    create
    1.新增VPN连接统计页
    2.修改VPN请求调用逻辑

---
+++ mobplus-admin-web/src/views/dataManager/vpnAccount-log.vue
@@ -0,0 +1,220 @@
+<template>
+    <div>
+        <a-row :gutter="12">
+            <a-col :span="4">
+                <a-card :bordered="true" title="应用信息" class="tree-list" :loading="treeloading">
+                    <a-icon
+                        slot="extra"
+                        type="sync"
+                        @click="loadAppTreeData"
+                        style="cursor: pointer"
+                    />
+                    <s-tree :dataSource="appTreeData" @add="onQueryDict" />
+                </a-card>
+            </a-col>
+            <a-col :span="20">
+                <a-card :bordered="true" class="card-list">
+                    <!--       -->
+                    <a-form class="act-bar" :form="form" id="form" ref="form" layout="inline">
+                        <div class="l" v-action="['dataManager:query']" v-if="currentApp!=null">
+                            <a-form-item>
+                                <a-input
+                                    type="text"
+                                    placeholder="请输入用户ip"
+                                    v-model="queryParam.userIp"
+                                />
+                            </a-form-item>
+                            <a-form-item>
+                                <a-input
+                                    type="text"
+                                    placeholder="请输入国家"
+                                    v-model="queryParam.country"
+                                />
+                            </a-form-item>
+                            <a-form-item>
+                                <a-input
+                                    type="text"
+                                    placeholder="请输入城市"
+                                    v-model="queryParam.city"
+                                />
+                            </a-form-item>
+                            <a-form-item>
+                                <a-input
+                                    type="text"
+                                    placeholder="请输入节点名"
+                                    v-model="queryParam.serverName"
+                                />
+                            </a-form-item>
+                            <a-form-item>
+                                <a-range-picker @change="onChangeDate"/>
+                            </a-form-item>
+                            <a-form-item>
+                                <div class="r" v-if="currentApp!=null">
+                                    <a-button-group>
+                                        <a-button
+                                            icon="sync"
+                                            v-action="['dataManager:query']"
+                                            @click="$refs.table.refresh(false)"
+                                        />
+                                        <a-button
+                                            type="primary"
+                                            icon="search"
+                                            @click="$refs.table.refresh(true)"
+                                        >查询</a-button>
+                                    </a-button-group>
+                                </div>
+                            </a-form-item>
+                        </div>
+                    </a-form>
+                    <!--       -->
+                    <s-table
+                        bordered
+                        ref="table"
+                        class="card-table"
+                        :columns="columns"
+                        :data="loadData"
+                        :lazy="true"
+                        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+                    >
+                        <template slot="timeSlot" slot-scope="text">
+                            {{ text | moment }}
+                        </template>
+                    </s-table>
+                </a-card>
+            </a-col>
+        </a-row>
+    </div>
+</template>
+
+<script>
+import { mixinDevice } from '@/utils/mixin';
+import { STable, STree, ETag } from '@/components';
+
+const columns = [
+    {
+        title: 'ip地址',
+        dataIndex: 'userIp'
+    },
+    {
+        title: '所属国家',
+        dataIndex: 'country'
+    },
+    {
+        title: '所属城市',
+        dataIndex: 'city'
+    },
+    {
+        title: '访问节点名',
+        dataIndex: 'serverName'
+    },
+    {
+        title: '连接日期',
+        dataIndex: 'createdAt',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.createdAt - b.createdAt,
+        scopedSlots: { customRender: 'timeSlot' }
+    }
+];
+
+const url = '/data/vpn';
+export default {
+    mixins: [mixinDevice],
+    components: {
+        STable,
+        STree,
+        ETag
+    },
+    data () {
+        return {
+            form: this.$form.createForm(this),
+            advanceSearch: false,
+            // 查询参数
+            queryParam: {},
+            // 表头
+            columns,
+            // 选中记录
+            selectedRowKeys: [],
+            selectedRows: [],
+            // 加载数据方法
+            loadData: this.loadDataList,
+            // 分类树数据
+            currentApp: null,
+            treeloading: false,
+            appTreeData: []
+        };
+    },
+    created () {},
+    mounted () {
+        this.loadAppTreeData();
+    },
+    computed: {},
+    methods: {
+        onChangeDate (date, dateString) {
+            this.queryParam.startTimeStr = dateString[0] + ' 00:00:00';
+            this.queryParam.endTimeStr = dateString[1] + ' 23:59:59';
+        },
+
+        onDelete: function (record) {
+            var params = [];
+            if (record !== undefined) {
+                params.push(record.id);
+            }
+            this.$confirm({
+                title: '确认删除吗?',
+                okText: '确认',
+                cancelText: '取消',
+                onOk: async () => {
+                    try {
+                        const data = await this.$http.delete(url, params);
+                        if (data) {
+                            this.$message.success(data);
+                            this.$refs.table.refresh(true);
+                        }
+                    } catch (err) {}
+                }
+            });
+        },
+        onSelectChange: function (selectedRowKeys, selectedRows) {
+            this.selectedRowKeys = selectedRowKeys;
+            this.selectedRows = selectedRows;
+        },
+        loadDataList: function (params) {
+            return this.$http.get(
+                url + '?appId=' + this.currentApp.key,
+                Object.assign(params, this.queryParam)
+            );
+        },
+        onQueryDict: function (item) {
+            this.currentApp = item;
+            this.$refs.table.refresh(true);
+        },
+        loadAppTreeData: async function () {
+            this.treeloading = true;
+            try {
+                const result = await this.$http.get(
+                    '/app/entity/item',
+                    { }
+                );
+                this.appTreeData = this.convertTreeData(result.data);
+            } finally {
+                this.treeloading = false;
+            }
+        },
+        convertTreeData: function (data) {
+            return data.map(ele => {
+                const item = {
+                    title: ele.name,
+                    key: ele.id
+                };
+                if (ele.children) {
+                    item.children = this.convertTreeData(ele.children);
+                }
+                return item;
+            });
+        }
+    }
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ mobplus-admin/src/main/java/com/yeecloud/adplus/admin/util/OkHttpUtils.java
@@ -0,0 +1,84 @@
+package com.yeecloud.adplus.admin.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.yeecloud.adplus.admin.controller.app.AppVPNController;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author: Leonard
+ * @create: 2021/4/28
+ */
+public class OkHttpUtils {
+
+    public final static String VPN_URL = "https://api.turbovpns.com";
+//    public final static String VPN_URL = "http://localhost:9092";
+
+    public static JSONObject getGETResponse(Request request, boolean data) throws IOException {
+        Response response = OkHttpUtils.buildNoVerifyClient().newCall(request).execute();
+        JSONObject jsonObject = JSONObject.parseObject(response.body().string());
+        return getDataResult(jsonObject);
+    }
+
+    public static JSONObject getGETResponse(Request request) throws IOException {
+        Response response = OkHttpUtils.buildNoVerifyClient().newCall(request).execute();
+        return JSONObject.parseObject(response.body().string());
+    }
+
+    public static OkHttpClient buildNoVerifyClient() {
+        OkHttpClient.Builder mBuilder = new OkHttpClient.Builder();
+        mBuilder.sslSocketFactory(createSSLSocketFactory());
+        mBuilder.hostnameVerifier(new TrustAllHostnameVerifier());
+        return mBuilder.build();
+    }
+
+    public static JSONObject getDataResult(JSONObject object) {
+        if (null == object) {
+            return null;
+        }
+        JSONObject data = new JSONObject();
+        data.put("data", object.getJSONArray("result"));
+        return data;
+    }
+
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null,  new TrustManager[] { new TrustAllCerts() }, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
+
+    private static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {return new X509Certificate[0];}
+    }
+
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+}
