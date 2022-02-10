bed54125745c74024f723c1e7746ed7439167de0
BlackAndW <41153009+BlackAndW@users.noreply.github.com>
2022-02-10 16:32:45 +0800

    update pom

--- mobplus-gateway/src/main/java/com/yeecloud/adplus/gateway/service/impl/AppPositionServiceImpl.java
+++ mobplus-gateway/src/main/java/com/yeecloud/adplus/gateway/service/impl/AppPositionServiceImpl.java
@@ -15,7 +15,6 @@ import com.yeecloud.adplus.gateway.service.AppPositionService;
 import com.yeecloud.meeto.common.exception.ServiceException;
 import com.yeecloud.meeto.common.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
